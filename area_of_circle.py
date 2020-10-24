fc3cce8e437cb4da5f6482307039b241a271029f
Anki-the-monkey <57085894+Anki-the-monkey@users.noreply.github.com>
2020-10-24 13:30:47 +0530

    Create area_of_circle.py

---
+++ area_of_circle.py
@@ -0,0 +1,10 @@
+class Circle():
+    pi = 3.14
+    def __init__(self,radius = 1):
+        self.radius = radius
+
+    def area(self):
+        return self.radius*self.radius*Circle.pi
+
+my_circle = Circle()
+print(my_circle.area())
